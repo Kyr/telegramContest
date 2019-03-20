21ccffedb06fa88a972bb86735df1cfee774676b
Kyrylo Fedorov <kyrylofedorov@gmail.com>
2019-03-20 08:48:45 +0200

    WIP: Fix tooltip

--- src/displayLines/displayHoverAbscissa.js
+++ src/displayLines/displayHoverAbscissa.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-export default DisplayTooltip;
+export default DisplayHoverAbscissa;
 
-function DisplayTooltip ({x, xLabel, height, dots}) {
+function DisplayHoverAbscissa ({x, xLabel, height, dots}) {
   const d = `M ${x},0 V ${height}`;
 const ry = height/100;
-const displayDate = new Date(xLabel).toLocaleDateString();
+//const displayDate = new Date(xLabel).toLocaleDateString();
   return (
     <g>
       <path d={d} stroke="lightgrey"/>
-      <rect x={x+10} y={5} width={100} height={ry*20} rx={9} ry={ry*3} fill="white" stroke="lightgrey"/>
+      {/*<rect x={x+10} y={5} width={100} height={ry*20} rx={9} ry={ry*3} fill="white" stroke="lightgrey"/>*/}
       {/*<text x={x+15} y={ry*10} lengthAdjust="spacingAndGlyphs" textLength="100%">{displayDate}</text>*/}
-      <text x={x+15} y={ry*10}>{displayDate}</text>
+      {/*<text x={x+15} y={ry*10}>{displayDate}</text>*/}
+{/*
       {
         dots.map(([name, stroke, y], i) => (
 <>
@@ -21,6 +22,7 @@ const displayDate = new Date(xLabel).toLocaleDateString();
         ))
       }
 
+*/}
       {
         dots.map(([name, stroke, y]) => (
           <ellipse key={name} stroke={stroke} cx={x} cy={y} rx="3" ry={ry} fill="white"/>
